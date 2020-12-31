bb1e1e97fea2c3003e0d57d7aa2ee0ab1685acee
Nguyễn Tri Mẫn <74088712+NguyenTriMan@users.noreply.github.com>
2020-12-31 18:11:33 +0700

    Upload file

---
+++ camera.py
@@ -0,0 +1,17 @@
+import urllib
+import cv2
+import numpy as np
+
+
+url = 'http://192.168.43.200:4747/video'
+
+cap = cv2.VideoCapture(url)
+
+while True:
+  ret, frame = cap.read()
+  cv2.imshow('Image', frame)
+  
+  if cv2.waitKey(1) & 0xFF == ord('q'):  #push 'q' to exit
+    cv2.imwrite('C:/Users/Admin/Documents/pythonVisualcode/image.jpg',frame)
+    break
+     
